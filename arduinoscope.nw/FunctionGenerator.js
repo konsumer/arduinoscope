1bd0312c700ca68b9056e7d62419ba7e7fb253cb
David Konsumer <konsumer@jetboystudio.com>
2013-02-06 08:00:05 -0800

    added function generator

---
+++ arduinoscope.nw/FunctionGenerator.js
@@ -0,0 +1,56 @@
+/**
+ * Each function will generate 127 point single-duty WAVE OF TYPE
+ */
+
+var FunctionGenerator=function(position){
+    // run a function on all points in array
+    this.func = function(f){
+        var out = [], x;
+        for (x=0; x<127; x++){
+            out.push(f(x));
+        }
+        return out;
+    };
+
+    this.noi = function(){
+        return this.func(function(x){
+            return Math.random();
+        });
+    };
+
+    this.sqr = function(){
+        return this.func(function(x){
+            return (x<64) ? 0 : 1;
+        });
+    };
+
+    this.saw = function(){
+        return this.func(function(x){
+            return x/127;
+        });
+    };
+
+    // would like to get this in other format...
+    this.tri = function(){
+        var out = [], i;
+        for (i=0; i<64; i++){
+            out.push(i/64);
+        }
+        for (i=64; i>0; i--){
+            out.push(i/64);
+        }
+        return out;
+    };
+
+
+    // STILL NEEDS WORK...
+
+    this.sin = function(){
+        return this.func(function(x){
+            return (Math.sin(x*2*Math.PI/180)+1)/2;
+        });
+    };
+
+
+
+};
--- arduinoscope.nw/oscilloscope.js
+++ arduinoscope.nw/oscilloscope.js
@@ -3,102 +3,109 @@
  */
 
 var Oscilloscope = function(pin, canvas, color, lineColor){
-	var scope = this;
+    var scope = this;
 
-	this.color = color || 'rgba(255,0,0,0.5)';
-	this.lineColor = lineColor || '#000';
-	
-	this.scaleX=1;
-	this.scaleY=1;
+    this.color = color || 'rgba(255,0,0,0.5)';
+    this.lineColor = lineColor || '#000';
 
-	// public
-	this.pin = pin;
-	this.canvas = canvas;
+    this.scaleX=1;
+    this.scaleY=1;
 
-	// private
-	var values = [];
-	var ctx = ctx = canvas.getContext("2d");
-	
-	ctx.globalAlpha = 1;
+    // public
+    this.pin = pin;
+    this.canvas = canvas;
+
+    // private
+    var values = [];
+    var ctx = canvas.getContext("2d");
+
+    ctx.globalAlpha = 1;
     ctx.lineWidth   = 1;
 
-	/**
-     * update canvas. should not have to do manual, as setters call this
-     * included for decoupling data from graphics, later
+    /**
+     * update canvas
      */
     this.update = function(){
-    	ctx.clearRect(0, 0, scope.canvas.width, scope.canvas.height);
-	    
-		// cross-hairs
-	    ctx.strokeStyle = scope.lineColor;
-	    ctx.beginPath();
-	    ctx.moveTo(canvas.width/2, 0);
-	    ctx.lineTo(canvas.width/2, canvas.height);
-	    ctx.moveTo(0, canvas.height/2);
-	    ctx.lineTo(canvas.width, canvas.height/2);
-	    ctx.stroke();
-	    ctx.closePath();
+        ctx.clearRect(0, 0, scope.canvas.width, scope.canvas.height);
 
-	    // values
-	    ctx.beginPath();
-	    ctx.strokeStyle = scope.color;
-	    ctx.moveTo(0, 0);
-	    scope.values.forEach(function(val, x){
-	    	ctx.lineTo(x * scope.scaleX, val * scope.scaleY * canvas.height);
-	    });
-		ctx.stroke();
-		ctx.closePath();
-	    
-    }
+        // cross-hairs
+        ctx.strokeStyle = scope.lineColor;
+        ctx.beginPath();
 
-	// getter/setters to do magic
-	// 
-	/**
+        // ctx.moveTo(canvas.width/2, 0);
+        // ctx.lineTo(canvas.width/2, canvas.height);
+        ctx.moveTo(0, canvas.height/2);
+        ctx.lineTo(canvas.width, canvas.height/2);
+
+        ctx.stroke();
+        ctx.closePath();
+
+        // values
+        ctx.beginPath();
+        ctx.strokeStyle = scope.color;
+        ctx.moveTo(0, canvas.height/2);
+        scope.values.forEach(function(val, x){
+            ctx.lineTo(x * scope.scaleX, val * scope.scaleY * canvas.height);
+        });
+        ctx.stroke();
+        ctx.closePath();
+
+    };
+
+    // getter/setters to do magic
+
+    /**
      * get min value
      */
-	this.__defineGetter__("min", function(){
+    this.__defineGetter__("min", function(){
         return this.values.sort().shift();
     });
 
     /**
      * get max value
      */
-	this.__defineGetter__("max", function(){
+    this.__defineGetter__("max", function(){
         return this.values.sort().pop();
     });
 
     /**
      * Interface for getting last value
      */
-	this.__defineGetter__("value", function(){
+    this.__defineGetter__("value", function(){
         return values[values.length-1];
     });
-    
+
     /**
      * Push single value to values, update()
      */
     this.__defineSetter__("value", function(val){
-        values.push(val);
-        this.update();
+        // quicker typeof
+        if (val['__proto__'] == Array.prototype){
+            val.forEach(function(v){
+                values.push(v);
+            });
+        }else{
+            values.push(val);
+        }
     });
 
     /**
      * Trim values to this.framesize
      */
-	this.__defineGetter__("values", function(){
-		if (values.length >= this.canvas.width){
-        	return values.slice(values.length-canvas.width,values.length);
-    	}else{
-    		return values.slice(0);
-    	}
+    this.__defineGetter__("values", function(){
+        // slice values to return copy
+        if (values.length >= this.canvas.width){
+            return values.slice(values.length-canvas.width,values.length);
+        }else{
+            return values.slice(0);
+        }
     });
-    
+
     /**
      * Interface for setting all values at once
      */
     this.__defineSetter__("values", function(val){
         values = val.slice(this.canvas.width);
-        this.update();
     });
 
     // update on init
